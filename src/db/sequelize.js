6de41508b092cd4a4ab9bb2c1657633f84e39783
ejavierP <elvinjavier7@gmail.com>
2024-05-15 08:32:57 -0400

    Agregando client sql

---
+++ src/db/sequelize.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+require('dotenv').config()
+
+
+class SequelizeClient {
+  constructor() {
+    const dialect = process.env.SEQUELIZE_DIALECT;
+    const username = process.env.SEQUELIZE_USERNAME;
+    const password = process.env.SEQUELIZE_PASSWORD;
+    const database = process.env.SEQUELIZE_DATABASE;
+
+    const host = process.env.SEQUELIZE_HOST;
+
+    this.sequelize = new Sequelize(database, username, password, {
+      dialect: dialect,
+      host: host,
+      logging: false,
+    });
+
+  }
+}
+
+module.exports = { SequelizeClient };
